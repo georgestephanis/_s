1e3da0952cc06d519181760028fdc920da1581ae
George Stephanis <georgestephanis@automattic.com>
2014-07-15 23:30:02 -0400

    Add the site description to the front page only.

--- header.php
+++ header.php
@@ -25,7 +25,6 @@
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</div>
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
@@ -33,5 +32,9 @@
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
+
+	<?php if ( is_front_page() ) : ?>
+		<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+	<?php endif; ?>
 
 	<div id="content" class="site-content">
